f1d185d288ad030fabb219652a55c31b7f6ab63c
Igor <morev.i@mail.ru>
2024-06-15 01:11:56 +1000

    fix: fix build

--- src/api/fetchExpoList.ts
+++ src/api/fetchExpoList.ts
@@ -2,7 +2,8 @@ import { ExpoDto } from './types';
 import { promises as fs } from 'fs';
 
 export async function fetchExpoList() {
-  const file = await fs.readFile(process.cwd() + '/src/api/expo.json', 'utf8');
+  console.log(process.cwd());
+  const file = await fs.readFile(`${process.cwd()}/public/expo.json`, 'utf8');
 
   const result = JSON.parse(file) as ExpoDto[];
 
